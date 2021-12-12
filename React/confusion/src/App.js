89e9c48f4bde1d5791d798e89ee051f1b6b8439a
Faizan Mirza <prime.wrecker797@gmail.com>
2021-12-12 19:17:26 +0500

    React

---
+++ React/confusion/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
+function App() {
+  return (
+    <div>
+      <Navbar dark color="secondary">
+          <div className="container col-12">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+    </div>
+  );
+}
+
+export default App;
