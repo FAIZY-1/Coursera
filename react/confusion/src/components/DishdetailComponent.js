38b92dddea57a9c107472eb9ccc2e991f1d42992
Faizan Mirza <prime.wrecker797@gmail.com>
2021-12-25 15:14:22 +0500

    Assignment 2

--- react/confusion/src/components/DishdetailComponent.js
+++ react/confusion/src/components/DishdetailComponent.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
     const DishDetail = (props) => {
-        if (this.props.dishr != null) {
+        if (this.props.dish != null) {
             return (
-                <div className="container">
+                    <div className="container">
                     <div className="row">
-                        <div className="col-md-5 col-12 m-1">
-                            {RenderDish(this.props.dishr)}
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>                
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderDish dish={props.dish} />
                         </div>
-                        <div className="col-md-5 col-12 m-1">
-                            {RenderComments(this.props.dishr.comments)}
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderComments comments={props.comments} />
                         </div>
                     </div>
-                </div>
+                    </div>
             );
         }
         else
--- react/confusion/src/components/MainComponent.js
+++ react/confusion/src/components/MainComponent.js
@@ -1,33 +1,61 @@
 import React, { Component } from 'react';
-import Menu from './MenuComponent';
-import DishDetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Menu from './MenuComponent';
+import Contact from './ContactComponent';
+import Home from './HomeComponent';
+import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
+import { DISHES } from '../shared/dishes';
+import {COMMENTS} from '../shared/comments';
+import {LEADERS} from '../shared/leaders';
+import {PROMOTIONS} from '../shared/promotions';
+import {Switch, Route, Redirect} from 'react-router-dom';
+
 class Main extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
         dishes: DISHES,
-        selectedDish: null
+        leaders: LEADERS,
+        promotions: PROMOTIONS,
+        comments: COMMENTS
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
-
   render() {
-    return (
+    const HomePage = () => {
+      return(
+          <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
+      );
+    }
+
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    }
+    
+    return(
       <div>
-        <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dishr={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-        <Footer />
+        <Header/>
+          <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/aboutus' component={About}/>
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route path='/menu/:dishId' component={DishWithId}/>
+              <Route exact path='/contactus' component={Contact}/>
+              <Redirect to="/home" />
+          </Switch>
+          <Footer/>
       </div>
-    );
+    )
   }
 }
-
+          
 export default Main;
